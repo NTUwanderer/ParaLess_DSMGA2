90d477517f9cc68bda17ad064e0857a308632f08
daydreamer <terry160434@gmail.com>
2017-06-07 08:36:06 +0800

    Add pyramid

---
+++ pyramid.cpp
@@ -0,0 +1,35 @@
+#include "pyramid.h"
+
+/*
+    Initialization of the pyramid structure
+
+    Build one choromsome in the population first.
+*/
+Pyramid :: Pyramid (int n_ell, int n_maxFe, int fffff)
+{   ell = n_ell;
+    maxFe = n_maxFe;
+    
+    Chromosome::length = n_ell;
+    Chromosome::lengthLong = quotientLong(ell)+1;
+    Chromosome::function = (Chromosome::function) fffff;
+    Chromosome::nfe = 0;
+    Chromosome::lsnfe = 0;
+    Chromosome::hitnfe = 0;
+    Chromosome::hit = false;
+
+    graph.init(ell);
+
+    bestIndex = -1; // Not found
+    masks = new list<int>[ell];
+    orderELL = new int[ell];
+
+    Chromosome ch;
+    ch.initR();     // Random initialization
+    population.push_back(ch);
+
+    
+
+    nCurrent = 1;
+
+
+}
---
+++ pyramid.h
@@ -0,0 +1,37 @@
+#include <list>
+
+#include "chromosome.h"
+#include "statistics.h"
+#include "trimatrix.h"
+#include "doublelinkedlistarray.h"
+#include "fastcounting.h"
+
+class Pyramid {
+    public:
+        Pyramid (int n_ell, int n_maxFe, int fffff);
+        ~Pyramid ();
+
+    private:
+        int ell;                                    // Problem length
+        int nCurrent;                               // Population size
+        bool EQ;                                    // Equal covering
+        int maxFe;                                  // maximum toleration of NFE
+        int bestIndex;                              // The best performing chromosome population[bestIndex]
+
+        unordered_map<unsigned long, double> pHash; // Population Hash
+        
+        list<int> *masks;                           // Restricted Mixing masks
+        vector<int> selectionIndex;                 // Record of tournament selection
+        
+        // Random order purpose
+        vector<int> orderN;
+        int *orderELL;
+
+        vector<Chromosome>      population;         // The set of chromosomes.
+        vecotr<int>             layers;             // The layer index of the correspoding chromosome in the population
+
+
+        TriMatrix<double> graph;                    // TODO: Make an ILS graph for each layer.
+
+        Statistics stFitness;                       // Statistics to record the fitness of the population
+}
